6f16d0e11da3807a1b2828064a94cd44c0866437
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-10-07 21:02:19 -0300

    Added Axios Added Favicon Added Fonts Added Global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --background: #e5e5e5;
+    --red: #e52e4d;
+    --blue: #4F46bb;
+    --green: #33cc85;
+    --black: #302e45;
+    --white: #ffffff;
+  }
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html{
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px){
+      font-size: 87.5%;
+    }
+  }
+  body{
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+  body,input,textarea,button{
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+  }
+  
+  h1,h2,h3,h4,h5,h6,strong{
+    font-weight: 700;
+  }
+  button{
+    cursor: pointer;
+  }
+  
+`
